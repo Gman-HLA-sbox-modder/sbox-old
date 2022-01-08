dc4434ddae6bf98bbe1a7ce3bf32e63d5deac233
Ian Earley <gmanchewing@gmail.com>
2022-01-08 07:32:40 -0500

    fix spawn menu

--- code/ui/left/SpawnList.cs
+++ code/ui/left/SpawnList.cs
@@ -9,28 +9,28 @@ public partial class SpawnList : Panel
 
 	public SpawnList()
 	{
-		AddClass("spawnpage");
-		AddChild(out Canvas, "canvas");
+		AddClass( "spawnpage" );
+		AddChild( out Canvas, "canvas" );
 
 		Canvas.Layout.AutoColumns = true;
 		Canvas.Layout.ItemWidth = 100;
 		Canvas.Layout.ItemHeight = 100;
 
-		Canvas.OnCreateCell = (cell, data) =>
+		Canvas.OnCreateCell = ( cell, data ) =>
 		{
 			var file = (string)data;
-			var panel = cell.Add.Panel("icon");
-			panel.AddEventListener("onclick", () => ConsoleSystem.Run("spawn", "models/" + file));
-			panel.Style.BackgroundImage = Texture.Load($"/models/{file}_c.png", false);
+			var panel = cell.Add.Panel( "icon" );
+			panel.AddEventListener( "onclick", () => ConsoleSystem.Run( "spawn", "models/" + file ) );
+			panel.Style.BackgroundImage = Texture.Load( FileSystem.Mounted, $"/models/{file}_c.png", false );
 		};
 
-		foreach (var file in FileSystem.Mounted.FindFile("models", "*.vmdl_c.png", true))
+		foreach ( var file in FileSystem.Mounted.FindFile( "models", "*.vmdl_c.png", true ) )
 		{
-			if (string.IsNullOrWhiteSpace(file)) continue;
-			if (file.Contains("_lod0")) continue;
-			if (file.Contains("clothes")) continue;
+			if ( string.IsNullOrWhiteSpace( file ) ) continue;
+			if ( file.Contains( "_lod0" ) ) continue;
+			if ( file.Contains( "clothes" ) ) continue;
 
-			Canvas.AddItem(file.Remove(file.Length - 6));
+			Canvas.AddItem( file.Remove( file.Length - 6 ) );
 		}
 	}
 }
